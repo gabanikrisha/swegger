3d58c743a426e9a5c0a55e5d04354f36c4f30e49
krisha <gabanikrisha5@gmail.com>
2025-11-13 15:14:37 +0530

    swegger java commit

--- src/main/java/com/example/swegger/SwaggerController.java
+++ src/main/java/com/example/swegger/SwaggerController.java
@@ -2,12 +2,15 @@ package com.example.swegger;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/patients")
 public class SwaggerController
 {
+    private List<String> patients = new ArrayList<>(List.of("Ram", "Shyam", "Sita"));
+
     @GetMapping
     public List<String> getPatients() {
         return List.of("Ram", "Shyam", "Sita");
